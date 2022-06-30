82233f65d176d16fd9058b65529bd1daea7ab43b
Dennis Hermsmeier <d.hermsmeier@mittwald.de>
2022-06-30 15:36:24 +0200

    Update mw-entrypoint.sh

--- bin/mw-entrypoint.sh
+++ bin/mw-entrypoint.sh
@@ -9,17 +9,8 @@ isCommand() {
 [[ -z "${DEBUG}" ]] || set -x
 
 if [[ ! "$(id -u "${LINT_NAME}")" -eq "${LINT_ID}" ]] || [[ ! "$(id -g "${LINT_NAME}")" -eq "${LINT_ID}" ]]; then
-  TMP_HOME="/tmp/home-${LINT_NAME}"
-  mkdir -p "${TMP_HOME}"
-
-  usermod -d "${TMP_HOME}" "${LINT_NAME}"
-
   usermod -o -u "${LINT_ID}" "${LINT_NAME}"
   groupmod -o -g "${LINT_ID}" "${LINT_NAME}"
-
-  usermod -d "/home/${LINT_NAME}" "${LINT_NAME}"
-
-  rm -rf "${TMP_HOME}"
 fi
 
 if [ "${1#-}" != "$1" ]; then
